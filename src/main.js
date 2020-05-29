737d8114218403e2cda810c7df78acc6d75b32ea
Hosein Khansari <hrk.x11@gmail.com>
2020-05-29 14:48:50 +0430

    format messages

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import calc from '@hkh12/node-calc';
 
 const TELEGRAM_URL = `https://api.telegram.org/bot${process.env.TOKEN}`;
-const { APP_URL } = process.env;
+// eslint-disable-next-line prefer-destructuring
+const APP_URL = process.env.APP_URL;
 
 export function fetchAndLog(endpoint) {
   const response = UrlFetchApp.fetch(TELEGRAM_URL + endpoint);
@@ -10,18 +11,24 @@ export function fetchAndLog(endpoint) {
 }
 
 export function sendMessage(id, text) {
-  fetchAndLog(`/sendMessage?chat_id=${id}&text=${encodeURIComponent(text)}`);
+  fetchAndLog(`/sendMessage?chat_id=${id}&text=${encodeURIComponent(text)}&parse_mode=html`);
+}
+
+function handleCalc(id, expr) {
+  try {
+    const answer = calc(expr);
+    sendMessage(id, `${expr} = <b>${answer}</b>`);
+  } catch (error) {
+    sendMessage(id, `🚫 There is a problem with <code>${expr}</code>:
+<b>${error.message}</b>`);
+  }
 }
 
 export function doPost(e) {
   const contents = JSON.parse(e.postData.contents),
-    { message } = contents;
-  try {
-    const answer = calc(message.text);
-    sendMessage(message.chat.id, answer);
-  } catch (error) {
-    sendMessage(message.chat.id, `Error: ${error.message}`);
-  }
+    { message } = contents,
+    { chat: { id }, text: expr } = message;
+  handleCalc(id, expr);
 }
 
 export function setWebhook() {
