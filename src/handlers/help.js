97935ededf297ae71c08467fe9ebea26c158140d
Hosein Khansari <hrk.x11@gmail.com>
2020-06-02 18:06:51 +0430

    improve help text

--- src/handlers/help.js
+++ src/handlers/help.js
@@ -4,7 +4,7 @@ import { USERNAME } from '../constants';
 export function handleHelp(id) {
   sendMessage(id, `Hi! I'm your calculator in Telegram. You can use me in these ways:
 - <code>/calculate 2*2</code>
-- Or only <code>2*2</code> (only works in this chat)
+- Or only <code>2*2</code> (only works in the private chat)
 - Inline mode: <code>@${USERNAME} 2*2</code>
 Valid expressions contain <b>numbers, and +-*/^ operators</b>.
 
