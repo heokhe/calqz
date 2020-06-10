83f18930f907c70ab79946501a41757776014497
Hosein Khansari <hrk.x11@gmail.com>
2020-06-10 10:53:58 +0430

    Use safer functions for number manipulations instead of operators

--- src/quiz/numberManipulations.js
+++ src/quiz/numberManipulations.js
@@ -1,3 +1,5 @@
+import * as ops from '@hkh12/node-calc/lib/operators';
+
 const getOrder = x => {
   if (x === 0) return 0;
   if (x < 0) x = -x;
@@ -14,7 +16,7 @@ export function addZeros(x) {
   const o = getOrder(x),
     r = x / (10 ** o);
   if (o >= 3 && (10 * r) % 1 === 0)
-    return x * (Math.random() > 0.5 ? 10 : 0.1);
+    return ops.mul(x, Math.random() > 0.5 ? 10 : 0.1);
   return x;
 }
 
@@ -22,7 +24,7 @@ export function removeZeros(x) {
   const o = getOrder(x),
     r = x / (10 ** o);
   if (o <= -2 && (10 * r) % 1 === 0)
-    return x / (Math.random() > 0.5 ? 0.1 : 100);
+    return ops.div(x, Math.random() > 0.5 ? 0.1 : 100);
   return x;
 }
 
@@ -31,5 +33,5 @@ export function produceRandomValues(x) {
     fp = (o / 2) * (1 + Math.random()),
     p = o > 0 ? Math.ceil(fp) : Math.floor(fp),
     t = (Math.random() > 0.5 ? -1 : 1) * (10 ** p);
-  return x + t;
+  return ops.add(x, t);
 }
