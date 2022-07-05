abe5f12e04bc289b7acdc991161a4b038057ad6d
Hpsein Khansari <hrk.x11@gmail.com>
2022-07-05 18:00:43 +0430

    Remove redundant @ and Mefi's username from the help message

--- src/handlers/help.js
+++ src/handlers/help.js
@@ -5,14 +5,12 @@ export function handleHelp(id) {
   sendMessage(id, `Hi! I'm your calculator in Telegram. You can use me in these ways:
 - <code>/calculate 2*2</code>
 - Or only <code>2*2</code> (only works in the private chat)
-- Inline mode: <code>@${USERNAME} 2*2</code>
+- Inline mode: <code>${USERNAME} 2*2</code>
 Valid expressions contain <b>numbers, +-*/^ operators</b>, and functions:
 - sin(x), cos(x), cot(x), tan(x)
 - sqrt(x), cbrt(x)
 - abs(x), x! (or fact(x))
 - log(x) (base 10), ln(x) (base e)
 
-I can also help you to <b>generate math quizzes</b>: <code>/quiz 2*2</code>.
-
-As @Mefi22 said, <i>made with fucken pain.</i>`);
+I can also help you to <b>generate math quizzes</b>: <code>/quiz 2*2</code>.`);
 }
